c225058bb0255b6a78d50e5f14a7303ca265394b
sherpal8 <sherpalkorotana@icloud.com>
2019-12-03 13:59:28 +0000

    basic setup of tutorial code along

---
+++ README-api.md
@@ -0,0 +1,9 @@
+# Webpack and Babel configuration
+
+This is a code along with Mr Corey House an expert on React Redux on Plural Sight. The purpose of this study is to understand what happens under the hood when `npx create-react-app` is called.
+
+Also, it is to understand better as to the purpose of Webpack to minimise JS code. Also, the configuration of Webpack for the development environment. Later, the configuration of Webpack for the production environment will also be visited. Webpack is by the command `create-react-app`.
+
+Here, a deeper understanding was obtained to understand how does Babel work and its uses in compiling modern JS so that it is compatible with the variety of Web Browsers in the world today. Also, the importance of Babel to compile JSX into JS. Also, a visit to the Babel website showed just how useful Babel is as it allowed a much more simplified use of JSX for app development, where Babel compiles JSX to a very complex JS code that is browser readable.
+
+Also, the configuration of Babel was visited and this done in package.json
---
+++ webpack.config.dev.js
@@ -0,0 +1,51 @@
+// to configure webpack for development env --> must be in root folder
+const webpack = require("webpack"); // CommonJS style syntax as ES6 not yet supported by Node
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+process.env.NODE_ENV = "development"; // so babel plugin knows this is in dev mode
+
+// below is a JS object to be exported for Webpack config in dev env
+module.exports = {
+  mode: "development", //to disable prod only features
+  target: "web", // can be node / web (browser)
+  devTool: "cheap-module-source-map", // this tool allows us get source map for debudding in browser
+  entry: "./src/index", // i.e. ./src/index.js
+  output: {
+    // not technically necessary as webpack doesn't output code for dev. but, needed for config object
+    path: path.resolve(__dirname, "build"), // directory name
+    publicPath: "/", // URL in browser
+    filename: "bundle.js" // this is served from memory for the dev environment
+  },
+  devServer: {
+    // webpack server as an alternative to Express server
+    stats: "minimal",
+    overlay: true,
+    historyApiFallback: true, // all request be sent to index.html
+    disableHostCheck: true,
+    headers: {
+      "Access-Control-Allow-Origin": "*"
+    },
+    https: false
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+      favicon: "src/favicon.ico"
+    })
+  ],
+  modules: {
+    rules: [
+      // an array to handle different groups of files e.g. js/jsx, css
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      },
+      {
+        test: /(\.css)$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  }
+};
