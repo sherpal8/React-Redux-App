e5963b0519d49848a3ffb6cd1d0e7d3cb323f153
sherpal8 <sherpalkorotana@icloud.com>
2019-12-09 15:59:31 +0000

    use of useState() hook to allow for use of "state" inside of the functional component

--- src/components/courses/ManageCoursePage.jsx
+++ src/components/courses/ManageCoursePage.jsx
@@ -1,10 +1,21 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import { loadCourses } from "../../redux/actions/courseActions";
 import { loadAuthors } from "../../redux/actions/authorActions";
 import PropTypes from "prop-types";
+import CourseForm from "./CourseForm.jsx";
+import { newCourse } from "../../../tools/mockData";
 
-function ManageCoursePage({ courses, authors, loadCourses, loadAuthors }) {
+function ManageCoursePage({
+  courses,
+  authors,
+  loadCourses,
+  loadAuthors,
+  ...props
+}) {
+  // useState() hook allows use of state in functional component
+  const [course, setCourse] = useState({ ...props.course });
+  const [errors, setErrors] = useState({});
   // note: useEffect() hook takes function it will call as 1st arg.
   // its 2nd arg is an array of items to watch to call that function.
   // as the array arg is empty, it has the same effect as componentDidMount()
@@ -20,14 +31,12 @@ function ManageCoursePage({ courses, authors, loadCourses, loadAuthors }) {
       });
     }
   }, []);
-  return (
-    <div>
-      <h2>Manage Course</h2>
-    </div>
-  );
+
+  return <CourseForm course={course} errors={errors} authors={authors} />;
 }
 
 ManageCoursePage.propTypes = {
+  course: PropTypes.object.isRequired,
   authors: PropTypes.array.isRequired,
   courses: PropTypes.array.isRequired,
   loadCourses: PropTypes.func.isRequired,
@@ -37,6 +46,7 @@ ManageCoursePage.propTypes = {
 function mapStateToProps(state) {
   // debugger;
   return {
+    course: newCourse,
     courses:
       state.authors.length === 0
         ? []
