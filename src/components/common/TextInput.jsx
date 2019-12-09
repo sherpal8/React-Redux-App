4363ad96d525915b2933bfacc00f9ff1f73b4e61
sherpal8 <sherpalkorotana@icloud.com>
2019-12-09 15:03:34 +0000

    Set up of TextInput.jsx and SelectInput.jsx functional components. Then, to utilise these standardised functional components inside CourseForm.jsx

---
+++ src/components/common/TextInput.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const TextInput = ({ name, label, onChange, placeholder, value, error }) => {
+  const wrapperClass =
+    error && error.length > 0 ? "form-group" : "form-group has-error";
+
+  return (
+    <div className={wrapperClass}>
+      <label htmlFor={name}>
+        <div className="field">
+          <input
+            type="text"
+            name={name}
+            className="form-control"
+            placeholder={placeholder}
+            value={value}
+            onChange={onChange}
+          />
+          {error && <div className="alert alert-danger">{error}</div>}
+        </div>
+      </label>
+    </div>
+  );
+};
+
+TextInput.propTypes = {
+  name: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  placeholder: PropTypes.string,
+  value: PropTypes.string,
+  error: PropTypes.string
+};
+
+export default TextInput;
