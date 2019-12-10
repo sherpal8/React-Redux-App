4872bc364a8a327536b7c843d4181684704207b5
sherpal8 <sherpalkorotana@icloud.com>
2019-12-10 09:38:30 +0000

    add saveCourse() thunk, and also actions creators of createCourseSuccess() and updateCourseSuccess() depending on whether a course.id existant or not

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -10,6 +10,14 @@ export function loadCourseSuccess(courses) {
   return { type: types.LOAD_COURSES_SUCCESS, courses };
 }
 
+export function createCourseSuccess(course) {
+  return { type: types.CREATE_COURSE_SUCCESS, course };
+}
+
+export function updateCourseSuccess(course) {
+  return { type: types.UPDATE_COURSE_SUCCESS, course };
+}
+
 export function loadCourses() {
   return function(dispatch) {
     return courseApi
@@ -19,6 +27,21 @@ export function loadCourses() {
       })
       .catch(err => {
         throw err;
+      });
+  };
+}
+
+export function saveCourse(course) {
+  return function(dispatch, getState) {
+    return courseApi
+      .saveCourse()
+      .then(savedCourse => {
+        course.id
+          ? dispatch(updateCourseSuccess(savedCourse))
+          : dispatch(createCourseSuccess(savedCourse));
+      })
+      .catch(error => {
+        throw error;
       });
   };
 }
