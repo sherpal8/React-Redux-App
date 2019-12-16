a541e7beea793ed3bf6ab0807ae73fdb8612d278
sherpal8 <sherpalkorotana@icloud.com>
2019-12-16 12:32:38 +0000

    use module react-toastify to indicate process is complete

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,8 @@ import Header from "./common/Header.jsx";
 import CoursesPage from "./courses/CoursesPage.jsx";
 import PageNotFound from "./PageNotFound.jsx";
 import ManageCoursePage from "./courses/ManageCoursePage.jsx";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
   return (
@@ -19,6 +21,7 @@ const App = () => {
         <Route path="/course" component={ManageCoursePage} />
         <Route component={PageNotFound} />
       </Switch>
+      <ToastContainer autoClose={3000} hideProgressBar />
     </div>
   );
 };
