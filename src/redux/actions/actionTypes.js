acf07a42252f32644a1e2eddceb2d5ef8b759db9
sherpal8 <sherpalkorotana@icloud.com>
2019-12-17 10:49:14 +0000

    types constant for optimistic delete

--- src/redux/actions/actionTypes.js
+++ src/redux/actions/actionTypes.js
@@ -4,3 +4,6 @@ export const CREATE_COURSE_SUCCESS = "CREATE_COURSE_SUCCESS";
 export const UPDATE_COURSE_SUCCESS = "UPDATE_COURSE_SUCCESS";
 export const BEGIN_API_CALL = "BEGIN_API_CALL";
 export const API_CALL_ERROR = "API_CALL_ERROR";
+
+// be particular with the naming. avoid *SUCCESS* as it will be a contextual misnomer.
+export const DELETE_COURSE_OPTIMISTIC = "DELETE_COURSE_OPTIMISTIC";
