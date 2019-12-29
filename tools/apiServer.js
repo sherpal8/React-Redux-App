4871269590797f4b0423eff4a9c889dce54ed14b
sherpal8 <sherpalkorotana@icloud.com>
2019-12-29 17:57:28 +0000

    minor change to understand how Netlify works with regards to Port number

--- tools/apiServer.js
+++ tools/apiServer.js
@@ -58,7 +58,8 @@ server.post("/courses/", function(req, res, next) {
 server.use(router);
 
 // Start server
-const port = 3001;
+const port = process.env.NODE_ENV === "production" ? process.env.PORT : 3001;
+console.log(port, "porrtttttttt");
 server.listen(port, () => {
   console.log(`JSON Server is running on port ${port}`);
 });
